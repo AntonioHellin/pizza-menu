f71a563cfbfef402ad09213c010fb04e615c91a1
AntonioHellin <74407060+AntonioHellin@users.noreply.github.com>
2023-09-05 19:15:59 +0000

    React fragments

--- src/index.js
+++ src/index.js
@@ -70,16 +70,24 @@ function Header() {
 function Menu() {
   const pizzas = pizzaData;
   const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. Authentic Italian cuisine. Authentic
+            Italian cuisine. Authentic Italian cuisine. Authentic Italian
+            cuisine. Authentic Italian cuisine.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on out menu. Please come back later.</p>
       )}
