ffcf99ee7fe2f853d98e83ed1f56e074417e7cf0
AntonioHellin <74407060+AntonioHellin@users.noreply.github.com>
2023-09-05 18:37:46 +0000

    Rendering with multiple returns

--- src/index.js
+++ src/index.js
@@ -88,6 +88,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -105,6 +107,10 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (!isOpen) {
+  //   return <p>CLOSED.</p>;
+  // }
 
   return (
     <footer className="footer">
